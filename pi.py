59b0c4f7b85413b596fe39850a898707e495735b
Chad Roberts <croberts@redhat.com>
2016-08-26 15:46:26 -0400

    Initial version of pi.py

---
+++ pi.py
@@ -0,0 +1,21 @@
+import sys
+from pyspark import SparkConf
+from pyspark import SparkContext
+
+conf = SparkConf()
+conf.setAppName('spark-pi-s2i-crobby')
+sc = SparkContext(conf=conf)
+
+
+def sample(p):
+    x, y = random(), random()
+    return 1 if x*x + y*y < 1 else 0
+
+NUM_SAMPLES=sys.argv[1]
+
+count = sc.parallelize(xrange(0, NUM_SAMPLES)).map(sample).reduce(lambda a, b: a + b)
+print "Pi is still approximately %f" % (4.0 * count / NUM_SAMPLES)
+
+
+
+
