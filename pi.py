3b10db90ae4f1e8003135660b6993acb6eb7616f
Chad Roberts <croberts@redhat.com>
2016-08-29 15:37:35 -0400

    Tweaks to sparkify v1

--- pi.py
+++ pi.py
@@ -1,23 +1,17 @@
 from random import random
 import sys
-from pyspark import SparkConf
-from pyspark import SparkContext
-
+from pyspark.sql import SparkSession
 
 def sample(p):
     x, y = random(), random()
     return 1 if x*x + y*y < 1 else 0
 
 if __name__ == "__main__":
-    conf = SparkConf()
-    conf.setAppName('spark-pi-s2i-crobby')
-    sc = SparkContext(conf=conf)
-
-    NUM_SAMPLES=int(sys.argv[1])
-    print "Will be using %d samples." % (NUM_SAMPLES)
-    count = sc.parallelize(xrange(0, NUM_SAMPLES)).map(sample).reduce(lambda a, b: a + b)
-    print "Pi is still approximately %f" % (4.0 * count / NUM_SAMPLES)
-
-
-
+    spark = SparkSession.builder.appName("crobby-Pi-1").getOrCreate()
+    
+    parts = int(sys.argv[1]) if len(sys.argv) > 1 else 2
+    n = 100000 * parts
+    print "Will be using %d partitions." % (parts)
+    count = spark.sparkContext.parallelize(range(0, n), parts).map(sample).reduce(add)
+    print "Pi is still approximately %f" % (4.0 * count / n)
 
