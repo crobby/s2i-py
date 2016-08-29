219f67144599da7919d9678b7f31cd9937d3a2ea
Chad Roberts <croberts@redhat.com>
2016-08-29 14:52:18 -0400

    reorg to not let global stuff hose me

--- pi.py
+++ pi.py
@@ -3,20 +3,20 @@ import sys
 from pyspark import SparkConf
 from pyspark import SparkContext
 
-conf = SparkConf()
-conf.setAppName('spark-pi-s2i-crobby')
-sc = SparkContext(conf=conf)
-
 
 def sample(p):
     x, y = random(), random()
     return 1 if x*x + y*y < 1 else 0
 
 if __name__ == "__main__":
-  NUM_SAMPLES=int(sys.argv[1])
-  print "Will be using %d samples." % (NUM_SAMPLES)
-  count = sc.parallelize(xrange(0, NUM_SAMPLES)).map(sample).reduce(lambda a, b: a + b)
-  print "Pi is still approximately %f" % (4.0 * count / NUM_SAMPLES)
+    conf = SparkConf()
+    conf.setAppName('spark-pi-s2i-crobby')
+    sc = SparkContext(conf=conf)
+
+    NUM_SAMPLES=int(sys.argv[1])
+    print "Will be using %d samples." % (NUM_SAMPLES)
+    count = sc.parallelize(xrange(0, NUM_SAMPLES)).map(sample).reduce(lambda a, b: a + b)
+    print "Pi is still approximately %f" % (4.0 * count / NUM_SAMPLES)
 
 
 
