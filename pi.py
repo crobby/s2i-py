97213ed178aa3bde3022d3d231d3cc243a31d6bd
Chad Roberts <croberts@redhat.com>
2016-08-29 14:41:25 -0400

    __main__

--- pi.py
+++ pi.py
@@ -12,11 +12,11 @@ def sample(p):
     x, y = random(), random()
     return 1 if x*x + y*y < 1 else 0
 
-NUM_SAMPLES=int(sys.argv[1])
-print "Will be using %d samples." % (NUM_SAMPLES)
-
-count = sc.parallelize(xrange(0, NUM_SAMPLES)).map(sample).reduce(lambda a, b: a + b)
-print "Pi is still approximately %f" % (4.0 * count / NUM_SAMPLES)
+if __name__ == "__main__":
+  NUM_SAMPLES=int(sys.argv[1])
+  print "Will be using %d samples." % (NUM_SAMPLES)
+  count = sc.parallelize(xrange(0, NUM_SAMPLES)).map(sample).reduce(lambda a, b: a + b)
+  print "Pi is still approximately %f" % (4.0 * count / NUM_SAMPLES)
 
 
 
